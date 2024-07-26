2635c356f9dc7bef97cf4f53c4faf5b007066286
im-varun <vrun.mulchandani@gmail.com>
2024-07-25 19:33:08 -0500

    Add basic structure of functions that will calculate tf-idf measure

---
+++ tfidf.py
@@ -0,0 +1,10 @@
+import math
+
+def tf(word, document):
+    pass
+
+def idf(word, documents_list):
+    pass
+
+def tf_idf_score(word, document, documents_list):
+    return tf(word, document) * idf(word, documents_list)
