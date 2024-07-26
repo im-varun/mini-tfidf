6cf589b5a39ff78d8d659ecda42ff4f6e0316989
im-varun <vrun.mulchandani@gmail.com>
2024-07-25 19:38:51 -0500

    Add expression to calculate term frequency (tf)

--- tfidf.py
+++ tfidf.py
@@ -1,10 +1,10 @@
 import math
 
-def tf(word, document):
-    pass
+def tf(word, document_tokens):
+    return (document_tokens.count(word) / len(document_tokens))
 
 def idf(word, documents_list):
     pass
 
 def tf_idf_score(word, document, documents_list):
-    return tf(word, document) * idf(word, documents_list)
+    return (tf(word, document) * idf(word, documents_list))
