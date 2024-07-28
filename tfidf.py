64da65ed598e4fbddb03f0b0f4e7a4ce508bdd90
im-varun <vrun.mulchandani@gmail.com>
2024-07-28 13:10:30 -0500

    Adjust idf expression

--- tfidf.py
+++ tfidf.py
@@ -8,7 +8,7 @@ def num_documents_containing(word, documents):
     return sum(1 for document in documents if word in document.words)
 
 def idf(word, documents):
-    return math.log(len(documents) / (1 + num_documents_containing(word, documents)))
+    return math.log((1 + len(documents)) / (1 + num_documents_containing(word, documents)))
 
 def tf_idf_score(word, document, documents_list):
     return (tf(word, document) * idf(word, documents_list))
